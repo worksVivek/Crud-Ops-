f17a6aa13e17284a12b6e162397c441b7964c457
worksVivek <works.Vivek990@gmail.com>
2024-06-25 11:22:39 +0530

    Initial

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const app = express();
 const path = require('path');
 const userModel= require('./models/user');
 const { name } = require('ejs');
+const { rmSync } = require('fs');
 app.set("view engine","ejs");
 app.use(express.json());
 app.use(express.urlencoded({extended : true}));
@@ -11,19 +12,37 @@ app.use(express.static(path.join(__dirname,'public')));
 app.get('/',(req,res)=>{
     res.render("index");
 })
-app.get('/read',(req,res)=>{
-    let users = userModel.find();
+app.get('/read',async (req,res)=>{
+    let users = await userModel.find();
     res.render("read",{users});
     
 })
+
+app.get('/edit/:userid',async (req,res)=>{
+    let user = await userModel.findOne({_id: req.params.userid});
+    res.render("edit",{user});
+    
+})
+app.post('/update/:userid', async (req,res)=>{
+    let { name, email, image}= req.body;
+    let user = await userModel.findOneAndUpdate({_id : req.params.userid},{name,email,image}, {new:true});
+    res.redirect("/read");
+})
+
+app.get('/delete/:id', async (req,res)=>{
+   await userModel.findOneAndDelete({_id: req.params.id});
+   let users = await userModel.find();
+    res.render("read", {users});
+});
+
 app.post('/create',async (req,res)=>{
     let {name,email,image} = req.body;
-   let createdUser =await userModel.create({
+    let createdUser = await userModel.create({
         name,
         email,
         image
     });
-    res.send(createdUser)
+    res.redirect("/read");
 })
 
 app.listen(3000);
