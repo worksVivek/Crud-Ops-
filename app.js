a442c99064d1b8590f5d604643a3cfebbbe7ef03
worksVivek <works.Vivek990@gmail.com>
2024-06-28 16:22:15 +0530

    comment commit

--- app.js
+++ app.js
@@ -8,26 +8,26 @@ app.set("view engine","ejs");
 app.use(express.json());
 app.use(express.urlencoded({extended : true}));
 app.use(express.static(path.join(__dirname,'public')));
-
+// github tutorial
 app.get('/',(req,res)=>{
     res.render("index");
-})
+});
 app.get('/read',async (req,res)=>{
     let users = await userModel.find();
     res.render("read",{users});
     
-})
+});
 
 app.get('/edit/:userid',async (req,res)=>{
     let user = await userModel.findOne({_id: req.params.userid});
     res.render("edit",{user});
     
-})
+});
 app.post('/update/:userid', async (req,res)=>{
     let { name, email, image}= req.body;
     let user = await userModel.findOneAndUpdate({_id : req.params.userid},{name,email,image}, {new:true});
     res.redirect("/read");
-})
+});
 
 app.get('/delete/:id', async (req,res)=>{
    await userModel.findOneAndDelete({_id: req.params.id});
@@ -43,6 +43,6 @@ app.post('/create',async (req,res)=>{
         image
     });
     res.redirect("/read");
-})
+});
 
 app.listen(3000);
